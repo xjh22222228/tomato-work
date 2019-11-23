ce0a959546d893dc3d72743ef1754153b9fc1b26
xjh22222228 <xjh22222228@gmail.com>
2019-11-23 19:02:10 +0800

    feat: CapitalFlow add search

--- src/views/home/CapitalFlow/Index/index.tsx
+++ src/views/home/CapitalFlow/Index/index.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useCallback, useRef, useReducer } from 'react';
+import React, { useState, useEffect, useCallback, useRef, useReducer, Fragment } from 'react';
 import './style.scss';
-import { DatePicker, Button, Select, Tag, Statistic } from 'antd';
+import { DatePicker, Button, Select, Tag, Statistic, Input } from 'antd';
 import moment from 'moment';
 import {
   getCurMonthFirstDay,
@@ -17,12 +17,14 @@ import Table from '@/components/Table';
 import { OPTION_TYPES, TypeNames, TypeColors } from '../enum';
 import CreateCapitalFlow from '../components/CreateCapitalFlow';
 
+const { Search } = Input;
 const { RangePicker } = DatePicker;
 const Option = Select.Option;
 const dateFormat = 'YYYY-MM-DD';
 
 interface State {
   date: moment.Moment[];
+  searchKeyword: string,
   name: string;
   type: any;
   modalVisible: boolean;
@@ -34,6 +36,7 @@ interface State {
 
 const initialState: State = {
   date: [],
+  searchKeyword: '',
   name: '',
   type: '',
   modalVisible: false,
@@ -80,18 +83,22 @@ const Reminder: React.FC = function() {
   const initParams = useCallback(() => {
     const startDate = moment(getCurMonthFirstDay(dateFormat), dateFormat);
     const endDate = moment(getCurMonthLastDay(dateFormat), dateFormat);
-    setState({ name: '', date: [startDate, endDate] });
+    setState({ searchKeyword: '', name: '', date: [startDate, endDate] });
     setTimeout(() => {
       tableRef.current.getTableData();
     }, 100);
   }, [setState]);
 
   // 获取数据
-  const getCapitalFlow = useCallback((params: any = {}) => {
-    params.typeNameId = state.name;
-    params.type = state.type;
-    params.startDate = state.date[0].valueOf();
-    params.endDate = state.date[1].valueOf() + ONE_DAY_TIMESTAMP;
+  const getCapitalFlow = useCallback((params: any) => {
+    params = {
+      ...params,
+      keyword: state.searchKeyword,
+      typeNameId: state.name,
+      type: state.type,
+      startDate: state.date[0].valueOf(),
+      endDate: state.date[1].valueOf() + ONE_DAY_TIMESTAMP
+    };
 
     return serviceGetCapitalFlow(params).then(res => {
       if (res.data.success) {
@@ -114,7 +121,7 @@ const Reminder: React.FC = function() {
       }
       return res;
     });
-  }, [state.date, state.name, state.type, setState]);
+  }, [state.date, state.searchKeyword, state.name, state.type, setState]);
 
   // 获取所有类型
   const getCapitalFlowType = useCallback(() => {
@@ -185,17 +192,23 @@ const Reminder: React.FC = function() {
           ))
         }
         </Select>
-        <span>查询类型：</span>
-        <Select 
-          onChange={(value: string) => setState({ type: value })} 
-          value={state.type}
-        >
         {
-          OPTION_TYPES.map(item => (
-            <Option value={item.value} key={item.value}>{ item.name }</Option>
-          ))
+          !state.name && (
+            <Fragment>
+              <span>查询类型：</span>
+              <Select 
+                onChange={(value: string) => setState({ type: value })} 
+                value={state.type}
+              >
+              {
+                OPTION_TYPES.map(item => (
+                  <Option value={item.value} key={item.value}>{ item.name }</Option>
+                ))
+              }
+              </Select>
+            </Fragment>
+          )
         }
-        </Select>
         <span>日期：</span>
         <RangePicker
           format={dateFormat} 
@@ -203,21 +216,31 @@ const Reminder: React.FC = function() {
           value={state.date} 
           onChange={(date: any) => setState({ date })} 
         />
-        <Button type="primary" onClick={() => tableRef.current.getTableData() }>查询</Button>
-        <Button onClick={() => setState({ modalVisible: true, currentRow: null })}>新增</Button>
-        <Button onClick={initParams}>重置</Button>
+        <div>
+          <Search
+            value={state.searchKeyword}
+            placeholder="试试搜索备注"
+            maxLength={300}
+            onChange={e => setState({ searchKeyword: e.target.value })}
+            onSearch={() => tableRef.current.getTableData()}
+            style={{ width: 260, margin: '10px 15px 0 0' }}
+          />
+          <Button type="primary" onClick={() => tableRef.current.getTableData()}>查询</Button>
+          <Button onClick={() => setState({ modalVisible: true, currentRow: null })}>新增</Button>
+          <Button onClick={initParams}>重置</Button>
+        </div>
         <div className="poly">
           <div className="item-price">
-            <em>收入：</em>
-            <Statistic value={state.price.income} precision={2} />￥
+            <em>收入：￥</em>
+            <Statistic value={state.price.income} precision={2} />
           </div>
           <div className="item-price">
-            <em>支出：</em>
-            <Statistic value={state.price.consumption} precision={2} />￥
+            <em>支出：￥</em>
+            <Statistic value={state.price.consumption} precision={2} />
           </div>
           <div className="item-price">
-            <em>实际收入：</em>
-            <Statistic value={state.price.income - state.price.consumption} precision={2} />￥
+            <em>实际收入：￥</em>
+            <Statistic value={state.price.income - state.price.consumption} precision={2} />
           </div>
         </div>
       </div>

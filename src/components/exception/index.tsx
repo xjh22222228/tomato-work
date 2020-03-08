74a9fc7d8a735b8da17dac0ea6088f1882505e86
xjh22222228 <xjh22222228@gmail.com>
2020-03-08 09:42:26 +0800

    fix: exception ts

--- src/components/exception/index.tsx
+++ src/components/exception/index.tsx
@@ -4,25 +4,25 @@ import { Result, Button } from 'antd';
 import { withRouter, RouteComponentProps } from 'react-router-dom';
 
 interface Props {
-  status?: '403' | '404' | '500';
+  status?: 403 | 404 | 500;
 }
 
 const statusMap = {
-  '403': {
+  403: {
     title: '403',
     subTitle: 'Sorry, you are not authorized to access this page.',
   },
-  '404': {
+  404: {
     title: '404',
     subTitle: 'Sorry, the page you visited does not exist.',
   },
-  '500': {
+  500: {
     title: '500',
     subTitle: 'Sorry, the server is wrong.',
   }
 };
 
-const NoMatch: React.FC<Props & RouteComponentProps> = function ({ history, status = '404' }) {
+const NoMatch: React.FC<Props & RouteComponentProps> = function ({ history, status = 404 }) {
   return (
     <Result 
       status={status} 

4d84f2ca9cd37c47f1b6647f7c6581a8b781d982
xjh22222228 <xjh22222228@gmail.com>
2020-05-04 12:04:15 +0800

    fix: build

--- config/paths.js
+++ config/paths.js
@@ -15,10 +15,11 @@ const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 // single-page apps that may serve index.html for nested URLs like /todos/42.
 // We can't use a relative path in HTML because we don't want to load something
 // like /todos/42/static/js/bundle.7289d.js. We have to know the root.
+const _publicUrl = process.env.NODE_EV === 'development' ? '/' : 'https://xiejiahe.gitee.io/tomato-work/';
 const publicUrlOrPath = getPublicUrlOrPath(
   process.env.NODE_ENV === 'development',
   require(resolveApp('package.json')).homepage,
-  process.env.PUBLIC_URL
+  process.env.NODE_ENV === 'development' ? process.env.PUBLIC_URL : _publicUrl
 );
 
 const moduleFileExtensions = [

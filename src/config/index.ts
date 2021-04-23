7e57682249f180ec0cf891fb281dbadebe417087
xjh22222228 <xjh22222228@gmail.com>
2021-04-23 10:13:09 +0800

    fix: github callback

--- src/config/index.ts
+++ src/config/index.ts
@@ -17,7 +17,7 @@ const CONFIG = {
   github: {
     clientId: isProduction ? '789d87c19dd5ed1dc42e' : '489b39e1f91d934128c8',
     // callbackURL 不可随意更改, 否则需要与服务端配置文件一同修改
-    callbackURL: `${window.location.origin}/api/passport/github/callback`,
+    callbackURL: `${isProduction ? 'https://work-api.xiejiahe.com' : window.location.origin}/api/passport/github/callback`,
     repositoryUrl: 'https://github.com/xjh22222228/tomato-work',
     bug: 'https://github.com/xjh22222228/tomato-work/issues'
   }

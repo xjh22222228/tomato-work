45baf069e5fb048d85470c9d34e74f0b48e481ff
xjh22222228 <xjh22222228@gmail.com>
2021-10-08 13:53:09 +0800

    fix: moment locale

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux'
 import { ConfigProvider } from 'antd'
 import zh_CN from 'antd/lib/locale-provider/zh_CN'
 import moment from 'moment'
-import 'moment/locale/zh-cn'
+import 'moment/dist/locale/zh-cn'
 import store from '@/store'
 
 moment.locale('zh-cn')

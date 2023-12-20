a472d4a026ee7143d6a5b5bef11a15de0466a08c
xiejiahe <xjh22222228@gmail.com>
2023-12-20 19:50:37 +0800

    fix: middleware ts

--- src/store/middlewares.ts
+++ src/store/middlewares.ts
@@ -1,10 +1,10 @@
-import { Middleware, PayloadAction } from '@reduxjs/toolkit'
+import { Middleware } from '@reduxjs/toolkit'
 import { SET_USER_INFO } from '@/store/userSlice'
 import { LOCAL_STORAGE } from '@/constants'
 import { RootState } from '@/store/index'
 
 export const authMiddleware: Middleware<{}, RootState> =
-	(storeApi) => (next) => (action: PayloadAction) => {
+	() => (next) => (action: unknown) => {
 		if (SET_USER_INFO.match(action) && !!action.payload.token) {
 			localStorage.setItem(LOCAL_STORAGE.USER, JSON.stringify(action.payload))
 			localStorage.setItem(LOCAL_STORAGE.LOGIN_NAME, action.payload.loginName)
